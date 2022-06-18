5ea035e83c0fc34512484bda9ce62c43c8bb9d9a
gaoshaoyun <276185876@qq.com>
2022-06-18 14:54:25 +0800

    Update which-react.js

--- demo/which-react.js
+++ demo/which-react.js
@@ -7,20 +7,20 @@
 // } from "react";
 // import ReactDOM from "react-dom";
 
-import {
-  Component,
-  useReducer,
-  useState,
-  useEffect,
-  useLayoutEffect,
-} from "../src/react";
+// import {
+//   Component,
+//   useReducer,
+//   useState,
+//   useEffect,
+//   useLayoutEffect,
+// } from "../src/react";
 import ReactDOM from "../src/react-dom";
 
 export {
   ReactDOM,
-  Component,
-  useReducer,
-  useState,
-  useEffect,
-  useLayoutEffect,
+  // Component,
+  // useReducer,
+  // useState,
+  // useEffect,
+  // useLayoutEffect,
 };
