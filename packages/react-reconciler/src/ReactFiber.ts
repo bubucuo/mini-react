643416acf7f76b7504a09eb881cd528648a793f0
bubucuo <276185876@qq.com>
2023-03-01 18:56:00 +0800

    Update ReactFiber.ts

--- packages/react-reconciler/src/ReactFiber.ts
+++ packages/react-reconciler/src/ReactFiber.ts
@@ -1,144 +1,78 @@
-import {NoFlags} from "../src/ReactFiberFlags";
-import {IndeterminateComponent, WorkTag} from "../src/ReactWorkTags";
-import type {Fiber} from "../src/ReactInternalTypes";
+import {ReactElement} from "shared/ReactTypes";
+import {isFn} from "shared/utils";
+import {NoFlags} from "./ReactFiberFlags";
+import {Fiber} from "./ReactInternalTypes";
+import {isStr} from "../../shared/utils";
+import {Fragment, HostComponent} from "./ReactWorkTags";
+import {
+  IndeterminateComponent,
+  WorkTag,
+  ClassComponent,
+  FunctionComponent,
+} from "./ReactWorkTags";
 import {REACT_FRAGMENT_TYPE} from "shared/ReactSymbols";
 
-import {
-  ClassComponent,
-  Fragment,
-  FunctionComponent,
-  HostComponent,
-  HostText,
-} from "../src/ReactWorkTags";
-
-import {isFn, isStr} from "shared/utils";
-import {ReactElement, ReactFragment} from "shared/ReactTypes";
-
+// 创建一个fiber
 export function createFiber(
   tag: WorkTag,
   pendingProps: any,
   key: null | string,
-  returnFiber: Fiber
+  returnFiber: Fiber | null
 ): Fiber {
   return new FiberNode(tag, pendingProps, key, returnFiber);
 }
 
 function FiberNode(
   tag: WorkTag,
-  pendingProps: unknown,
+  pendingProps: any,
   key: null | string,
   returnFiber: Fiber
 ) {
   // Instance
+  // 标记组件类型
   this.tag = tag;
+  // 定义组件在当前层级下的唯一性
   this.key = key;
+  // 组件类型
   this.elementType = null;
+  // 组件类型
   this.type = null;
+  // 不同的组件的  stateNode 定义也不同
+  // 原生标签：string
+  // 类组件：实例
   this.stateNode = null;
 
   // Fiber
   this.return = returnFiber; //null;
   this.child = null;
   this.sibling = null;
+  // 记录了节点在兄弟节点中的位置下标，用于diff时候判断节点是否需要发生移动
   this.index = 0;
 
   this.pendingProps = pendingProps;
   this.memoizedProps = null;
   this.updateQueue = null;
+  // 不同的组件的 memoizedState 指代也不同
+  // 函数组件 hook0
+  // 类组件 state
   this.memoizedState = null;
 
   // Effects
   this.flags = NoFlags;
   this.subtreeFlags = NoFlags;
+  // 记录要删除的子节点
   this.deletions = null;
 
+  // 缓存fiber
   this.alternate = null;
 }
 
-// This is used to create an alternate fiber to do work on.
-export function createWorkInProgress(
-  current: Fiber,
-  pendingProps: any,
-  returnFiber: Fiber
-): Fiber {
-  let workInProgress = current.alternate;
-
-  if (workInProgress === null) {
-    workInProgress = createFiber(
-      current.tag,
-      pendingProps,
-      current.key,
-      returnFiber
-    );
-    workInProgress.elementType = current.elementType;
-    workInProgress.type = current.type;
-    workInProgress.stateNode = current.stateNode;
-
-    workInProgress.alternate = current;
-
-    current.alternate = workInProgress;
-  } else {
-    workInProgress.pendingProps = pendingProps;
-    // Needed because Blocks store data on type.
-    workInProgress.type = current.type;
-
-    // We already have an alternate.
-    // Reset the effect tag.
-    workInProgress.flags = NoFlags;
-
-    // The effects are no longer valid.
-    workInProgress.subtreeFlags = NoFlags;
-    workInProgress.deletions = null;
-  }
-
-  // Reset all effects except static ones.
-  // Static effects are not specific to a render.
-  workInProgress.flags = current.flags;
-
-  workInProgress.child = current.child;
-  workInProgress.memoizedProps = current.memoizedProps;
-  workInProgress.memoizedState = current.memoizedState;
-  workInProgress.updateQueue = current.updateQueue;
-
-  workInProgress.sibling = current.sibling;
-  workInProgress.index = current.index;
-
-  return workInProgress;
-}
-
-export function createFiberFromTypeAndProps(
-  type: any,
-  key: null | string,
-  pendingProps: any,
-  returnFiber: Fiber
-): Fiber {
-  let fiberTag: WorkTag = IndeterminateComponent;
-  // The resolved type is set if we know what the final type will be. I.e. it's not lazy.
-  if (isFn(type)) {
-    if (shouldConstruct(type)) {
-      fiberTag = ClassComponent;
-    } else {
-      fiberTag = FunctionComponent;
-    }
-  } else if (isStr(type)) {
-    fiberTag = HostComponent;
-  } else if (type === REACT_FRAGMENT_TYPE) {
-    return createFiberFromFragment(pendingProps.children, key, returnFiber);
-  }
-
-  const fiber = createFiber(fiberTag, pendingProps, key, returnFiber);
-  fiber.elementType = type;
-  fiber.type = type;
-
-  return fiber;
-}
-
+// 根据 ReactElement 创建Fiber
 export function createFiberFromElement(
   element: ReactElement,
   returnFiber: Fiber
-): Fiber {
-  const type = element.type;
-  const key = element.key;
+) {
+  const {type, key} = element;
   const pendingProps = element.props;
   const fiber = createFiberFromTypeAndProps(
     type,
@@ -149,24 +83,37 @@ export function createFiberFromElement(
   return fiber;
 }
 
-export function createFiberFromFragment(
-  elements: ReactFragment,
+// 更具 TypeAndProps 创建fiber
+export function createFiberFromTypeAndProps(
+  type: any,
   key: null | string,
+  pendingProps: any,
   returnFiber: Fiber
-): Fiber {
-  const fiber = createFiber(Fragment, elements, key, returnFiber);
+) {
+  let fiberTag: WorkTag = IndeterminateComponent;
+  if (isFn(type)) {
+    // 判断函数组件还是类组件
+    if (shouldConstruct(type)) {
+      fiberTag = ClassComponent;
+    } else {
+      fiberTag = FunctionComponent;
+    }
+  } else if (isStr(type)) {
+    // 原生标签
+    fiberTag = HostComponent;
+  } else if (type === REACT_FRAGMENT_TYPE) {
+    fiberTag = Fragment;
+  }
+
+  const fiber = createFiber(fiberTag, pendingProps, key, returnFiber);
+  fiber.elementType = type;
+  fiber.type = type;
   return fiber;
 }
 
-export function createFiberFromText(
-  content: string,
-  returnFiber: Fiber
-): Fiber {
-  const fiber = createFiber(HostText, content, null, returnFiber);
-  return fiber;
-}
-
+// 判断是否是类组件
 function shouldConstruct(Component: Function) {
   const prototype = Component.prototype;
+
   return !!(prototype && prototype.isReactComponent);
 }
